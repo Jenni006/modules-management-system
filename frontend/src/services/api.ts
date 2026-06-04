eae172ba61be880c304aed80af7198a700b8669f
Jenni006 <jennifer6kenzo@gmail.com>
2026-06-04 13:40:12 +0530

    Production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ,
+  baseURL: "https://modules-management-system.onrender.com" ,
   headers: { 'Content-Type': 'application/json' },
 });
+console.log("AXIOS BASE URL:", import.meta.env.VITE_API_URL);
 export default api;
